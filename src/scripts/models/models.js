a253e513618bc319c784c9695d437a1d8fec71b7
Justin Richards <richards.justind@gmail.com>
2016-07-13 16:50:32 -0500

    updated userModel

--- src/scripts/models/models.js
+++ src/scripts/models/models.js
@@ -24,15 +24,24 @@ export const UserModel = Backbone.Model.extend({
 				email: email,
 				password: password
 			}
-		})
+		}).then((email,password)=>this.login(email,password))
 	},
 	login: function(email,password) {
-
+		return $.ajax({
+			type: 'post',
+			url: '/auth/login',
+			data: {
+				email: email,
+				password: password
+			}
+		}).then((userData) => {
+			localStorage[APP_NAME] = JSON.stringify(userData)
+		})
 	},
 	logout: function() {
-
+		return $.getJSON('/auth/logout')
 	},
-	getCurrentUserId: function() {
-		
+	getCurrentUser: function() {
+		return JSON.parse(localStorage[APP_NAME])
 	}
 })
