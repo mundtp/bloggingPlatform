9e9d03c94f985f13c1b3925ed8e60b8e9e873b83
Philip Mundt <philipamundt@philipmndtsmbp2.attlocal.net>
2016-07-17 23:46:33 -0500

    deleted console log at login

--- src/scripts/actions.js
+++ src/scripts/actions.js
@@ -4,14 +4,12 @@ const ACTIONS = {
 	registerUser: function(email,password) {
 		console.log(email,password)
 		return User.register(email,password).then((resp) => {
-			console.log(resp)
 			return this.logUserIn(email,password)
 		})
 	},
 
 	logUserIn: function(email,password) {
 		return User.login(email,password).then(function(resp){
-			console.log(resp)
 			location.hash = "home"
 		})
 	},
